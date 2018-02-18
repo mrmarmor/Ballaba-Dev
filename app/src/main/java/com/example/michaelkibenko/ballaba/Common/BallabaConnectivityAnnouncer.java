cc3d6c1319384b58a1d401578dd2268193024ebb
Michael Kibenko <michael@ballaba-it.com>
2018-02-18 21:37:10 +0200

    New project started with connectivity announcer in and Navigation drawer

---
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Common/BallabaConnectivityAnnouncer.java
@@ -0,0 +1,86 @@
+package com.example.michaelkibenko.ballaba.Common;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+import java.io.Console;
+import java.util.ArrayList;
+
+/**
+ * Created by michaelkibenko on 18/02/2018.
+ */
+
+public class BallabaConnectivityAnnouncer {
+    public static BallabaConnectivityAnnouncer instance;
+
+    private boolean status;
+
+    private Context context;
+
+    private ArrayList<BallabaConnectivityListener> clients;
+
+    public static BallabaConnectivityAnnouncer getInstance(Context context) {
+        if(instance == null){
+            instance = new BallabaConnectivityAnnouncer(context);
+        }
+        return instance;
+    }
+
+    private BallabaConnectivityAnnouncer(Context context){
+        clients = new ArrayList<>();
+        this.context = context;
+        context.registerReceiver(new NetworkChangeReceiver(), new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
+    }
+
+    public boolean getStatus(){
+        return this.status;
+    }
+
+    public void register(BallabaConnectivityListener client){
+        if(clients != null) {
+            clients.add(client);
+        }
+    }
+
+    public void unRegister(BallabaConnectivityListener client){
+        if(clients != null){
+            if(clients.contains(client)){
+                clients.remove(client);
+            }
+        }
+    }
+
+    private void onConnectivityChange(boolean status){
+        for (BallabaConnectivityListener client : clients) {
+            client.onConnectivityChanged(status);
+        }
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(final Context context, final Intent intent) {
+            final ConnectivityManager connMgr = (ConnectivityManager) context
+                    .getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            final android.net.NetworkInfo wifi = connMgr
+                    .getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+
+            final android.net.NetworkInfo mobile = connMgr
+                    .getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+            if (wifi.isAvailable() || mobile.isAvailable()) {
+                status = true;
+            }else{
+                status = false;
+            }
+
+            onConnectivityChange(status);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Common/BallabaConnectivityListener.java
@@ -0,0 +1,9 @@
+package com.example.michaelkibenko.ballaba.Common;
+
+/**
+ * Created by michaelkibenko on 18/02/2018.
+ */
+
+public interface BallabaConnectivityListener {
+    public void onConnectivityChanged(boolean is);
+}
