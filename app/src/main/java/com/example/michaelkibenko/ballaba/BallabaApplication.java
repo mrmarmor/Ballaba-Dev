49707e475eefd6fd07312fff3f0ac0e64671d6c4
Michael Kibenko <michael@ballaba-it.com>
2018-02-19 10:31:49 +0200

    BallabaAplication

---
+++ app/src/main/java/com/example/michaelkibenko/ballaba/BallabaApplication.java
@@ -0,0 +1,15 @@
+package com.example.michaelkibenko.ballaba;
+
+import android.app.Application;
+
+/**
+ * Created by michaelkibenko on 19/02/2018.
+ */
+
+public class BallabaApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
