d9b81d08dff63ee352647f39349ecbfa492fff04
Michael Kibenko <michael@ballaba-it.com>
2018-02-19 10:58:36 +0200

    Splash Activity component

---
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Activities/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.example.michaelkibenko.ballaba.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SlidingPaneLayout;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by michaelkibenko on 19/02/2018.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
