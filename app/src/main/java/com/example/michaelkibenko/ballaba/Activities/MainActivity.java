9f878df33fb30413bbda9a9227408a3c8722a39f
Michael Kibenko <michael@ballaba-it.com>
2018-02-21 11:47:11 +0200

    Splash screen completed

--- app/src/main/java/com/example/michaelkibenko/ballaba/Activities/MainActivity.java
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Activities/MainActivity.java
@@ -1,21 +1,13 @@
 package com.example.michaelkibenko.ballaba.Activities;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
-import com.example.michaelkibenko.ballaba.Common.BallabaConnectivityAnnouncer;
-import com.example.michaelkibenko.ballaba.Common.BallabaConnectivityListener;
 import com.example.michaelkibenko.ballaba.R;
 
 public class MainActivity extends AppCompatActivity
@@ -25,43 +17,43 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        BallabaConnectivityAnnouncer.getInstance(this).register(new BallabaConnectivityListener() {
-            @Override
-            public void onConnectivityChanged(boolean is) {
-                Toast.makeText(MainActivity.this, is+"", Toast.LENGTH_LONG).show();
-            }
-        });
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
+//
+//        BallabaConnectivityAnnouncer.getInstance(this).register(new BallabaConnectivityListener() {
+//            @Override
+//            public void onConnectivityChanged(boolean is) {
+//                Toast.makeText(MainActivity.this, is+"", Toast.LENGTH_LONG).show();
+//            }
+//        });
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
     }
 
     @Override
--- app/src/main/java/com/example/michaelkibenko/ballaba/Activities/SplashActivity.java
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Activities/SplashActivity.java
@@ -1,8 +1,19 @@
 package com.example.michaelkibenko.ballaba.Activities;
 
+import android.app.ActionBar;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.michaelkibenko.ballaba.R;
+import com.example.michaelkibenko.ballaba.databinding.SplashLayoutBinding;
 
 /**
  * Created by michaelkibenko on 19/02/2018.
@@ -10,9 +21,18 @@ import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private SplashLayoutBinding binder;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binder = DataBindingUtil.setContentView(this, R.layout.splash_layout);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            }
+        }, 4000);
     }
 
 }
