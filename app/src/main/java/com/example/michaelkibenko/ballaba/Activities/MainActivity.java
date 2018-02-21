751da85a2365ef3678f360c665e7ff8fe5e905f8
Michael Kibenko <michael@ballaba-it.com>
2018-02-21 12:06:21 +0200

    Main screen configured

--- app/src/main/java/com/example/michaelkibenko/ballaba/Activities/MainActivity.java
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Activities/MainActivity.java
@@ -17,43 +17,11 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
-//
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.addDrawerListener(toggle);
-//        toggle.syncState();
-//
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
-//
-//        BallabaConnectivityAnnouncer.getInstance(this).register(new BallabaConnectivityListener() {
-//            @Override
-//            public void onConnectivityChanged(boolean is) {
-//                Toast.makeText(MainActivity.this, is+"", Toast.LENGTH_LONG).show();
-//            }
-//        });
     }
 
     @Override
     public void onBackPressed() {
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        if (drawer.isDrawerOpen(GravityCompat.START)) {
-//            drawer.closeDrawer(GravityCompat.START);
-//        } else {
-//            super.onBackPressed();
-//        }
+
     }
 
     @Override
