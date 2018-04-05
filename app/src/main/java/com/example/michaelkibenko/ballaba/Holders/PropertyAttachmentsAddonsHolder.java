8cfa1d2c637853892bca99f8efafc3fbfa49705a
Michael Kibenko <michael@ballaba-it.com>
2018-04-06 00:17:20 +0300

    parsing titles to hebrew for attachments only

--- app/src/main/java/com/example/michaelkibenko/ballaba/Holders/PropertyAttachmentsAddonsHolder.java
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Holders/PropertyAttachmentsAddonsHolder.java
@@ -56,12 +56,24 @@ public class PropertyAttachmentsAddonsHolder {
             JSONArray electronics = jsonObject.getJSONArray("electronics");
             JSONArray attachments = jsonObject.getJSONArray("attachments");
 
+            //TODO delete it
+            JSONObject not_furnished = new JSONObject();
+            not_furnished.put("id" , "123455");
+            not_furnished.put("title", "not_furnished");
+            attachments.put(not_furnished);
+
+            JSONObject no_electronics = new JSONObject();
+            no_electronics.put("id" , "12355");
+            no_electronics.put("title", "no_electronics");
+            attachments.put(no_electronics);
+
+
 
             for (int i = 0; i < furniture.length(); i++) {
                 JSONObject currentObject = furniture.getJSONObject(i);
                 String id = currentObject.getString("id");
                 String title = currentObject.getString("title");
-                PropertyAttachmentAddonEntity entity = new PropertyAttachmentAddonEntity(id, title, title);
+                PropertyAttachmentAddonEntity entity = new PropertyAttachmentAddonEntity(id, title, getFormattedFurnitureTitle(title));
                 this.furniture.add(entity);
             }
 
@@ -69,7 +81,7 @@ public class PropertyAttachmentsAddonsHolder {
                 JSONObject currentObject = electronics.getJSONObject(i);
                 String id = currentObject.getString("id");
                 String title = currentObject.getString("title");
-                PropertyAttachmentAddonEntity entity = new PropertyAttachmentAddonEntity(id, title, title);
+                PropertyAttachmentAddonEntity entity = new PropertyAttachmentAddonEntity(id, title, getFormattedElectronicsTitle(title));
                 this.electronics.add(entity);
             }
 
@@ -77,13 +89,69 @@ public class PropertyAttachmentsAddonsHolder {
                 JSONObject currentObject = attachments.getJSONObject(i);
                 String id = currentObject.getString("id");
                 String title = currentObject.getString("title");
-                PropertyAttachmentAddonEntity entity = new PropertyAttachmentAddonEntity(id, title, title);
+                PropertyAttachmentAddonEntity entity = new PropertyAttachmentAddonEntity(id, title, getFormattedAttachmentsTitle(title));
                 this.attachments.add(entity);
             }
 
-            Log.e(TAG, "vfdvfd");
         }catch (JSONException ex){
             ex.printStackTrace();
         }
+    }
+
+    private String getFormattedFurnitureTitle(String title){
+        return title;
+    }
+
+    private String getFormattedAttachmentsTitle(String title){
+        switch (title){
+            case "furnished" :
+                return "מרוהטת";
+            case "not_furnished":
+                return "לא מרוהטת";
+            case "electronics" :
+                return "מוצרי חשמל";
+            case "no_electronics":
+                return "ללא מוצרי חשמל";
+            case "parking":
+                return "חנייה בבניין";
+            case "renovated":
+                return "משופצת";
+            case "sunboiler":
+                return "דוד שמש";
+            case "elevator":
+                return "מעלית";
+            case "guard":
+                return "שומר בבנין";
+            case "bars":
+                return "סורגים";
+            case "warehouse":
+                return "מחסן";
+            case "sunterrace":
+                return "מרפסת שמש";
+            case "service_balcony":
+                return "מרפסת שירות";
+            case "garden":
+                return "גינה";
+            case "pool":
+                return "בריכה";
+            case "animals":
+                return "אפשר בע״ח";
+            case "floor_heat":
+                return "חימום רצפתי";
+            case "all_included":
+                return "כולל הכל";
+            case "gym":
+                return "חדר כושר";
+            case "disabled_access":
+                return "גישה לנכים";
+            case "aps":
+                return "ממ״ד";
+
+                default:return title;
+        }
+    }
+
+    private String getFormattedElectronicsTitle(String title){
+        return title;
     }
 }
