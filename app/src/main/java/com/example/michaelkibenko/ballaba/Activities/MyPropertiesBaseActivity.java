f940669259c8d3fce8440ba347e286a7adda05c9
Michael Kibenko <michael@ballaba-it.com>
2018-05-21 19:13:36 +0300

    my property first component

---
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Activities/MyPropertiesBaseActivity.java
@@ -0,0 +1,14 @@
+package com.example.michaelkibenko.ballaba.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class MyPropertiesBaseActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
