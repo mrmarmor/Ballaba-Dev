c37a8ad499d310c7a091b016aadd6775e9e949dd
DESKTOP-76T2UMK\User\marmormoshe <moshe@ballaba-it.com>
2018-06-04 12:26:04 +0300

    editTexts fields validation completed

--- app/src/main/java/com/example/michaelkibenko/ballaba/Activities/CreditCardActivity.java
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Activities/CreditCardActivity.java
@@ -6,13 +6,18 @@ import android.databinding.DataBindingUtil;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.os.Message;
+import android.support.annotation.ColorInt;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.example.michaelkibenko.ballaba.Common.BallabaDialogBuilder;
@@ -36,7 +41,17 @@ import static java.sql.Types.NULL;
 public class CreditCardActivity extends BaseActivityWithActionBar implements View.OnFocusChangeListener{
     private static final String TAG = CreditCardActivity.class.getSimpleName();
 
-    ActivityCreditCardBinding binder;
+    @IntDef({ISRACARD, MASTERCARD, VISA, DINERS, AMERICAN_EXPRESS, NULL})
+    @interface CREDIT_CARD_ICON {
+        int ISRACARD = R.drawable.bathtub_blue_36;
+        int MASTERCARD = R.drawable.bed_blue_36;
+        int VISA = R.drawable.calender_blue_24_copy;
+        int DINERS = R.drawable.date_blue_36;
+        int AMERICAN_EXPRESS = R.drawable.exclusive;
+    }
+
+    private ActivityCreditCardBinding binder;
+    private int cardType;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -66,40 +81,96 @@ public class CreditCardActivity extends BaseActivityWithActionBar implements Vie
     }
 
     public void onClickNext(View view){
-        Toast.makeText(this, "go to next page", Toast.LENGTH_SHORT).show();
+        onFinish();
     }
 
-    @IntDef({ISRACARD, MASTERCARD, VISA, DINERS, AMERICAN_EXPRESS, NULL})
-    @interface CREDIT_CARD_ICON {
-        int ISRACARD = R.drawable.bathtub_blue_36;
-        int MASTERCARD = R.drawable.bed_blue_36;
-        int VISA = R.drawable.calender_blue_24_copy;
-        int DINERS = R.drawable.date_blue_36;
-        int AMERICAN_EXPRESS = R.drawable.exclusive;
-    }
     @Override
     public void onFocusChange(View v, boolean hasFocus) {
         if (!hasFocus) {
-            String s = binder.creditCardCardNumber.getText().toString().trim();
-            int cardType;
-            if (s.length() == 8 || s.length() == 9){
-                cardType = ISRACARD;
-            } else if (s.length() == 16 && (s.startsWith("51") || s.startsWith("52") || s.startsWith("53") || s.startsWith("54") || s.startsWith("55"))){
-                cardType = MASTERCARD;
-            } else if (s.length() == 16 && s.startsWith("4")) {
-                cardType = VISA;
-            } else if (s.length() == 14 && (s.startsWith("30") || s.startsWith("36") || s.startsWith("38"))) {
-                cardType = DINERS;
-            } else if (s.length() == 15 && (s.startsWith("27") || s.startsWith("37"))) {
-                cardType = AMERICAN_EXPRESS;
-            } else {
-                cardType = NULL;
-            }
+            @CREDIT_CARD_ICON int cardType = getCardType(binder.creditCardCardNumber.getText().toString().trim());
 
             //TESTING:
             //Log.d(TAG, "credit number: " + s + " ,card type: " + cardType);
 
             binder.creditCardCardNumber.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, cardType, 0);
         }
+    }
+
+    private @CREDIT_CARD_ICON int getCardType(String s) {
+        if (s.length() == 8 || s.length() == 9){
+            cardType = ISRACARD;
+        } else if (s.length() == 16 && (s.startsWith("51") || s.startsWith("52") || s.startsWith("53") || s.startsWith("54") || s.startsWith("55"))){
+            cardType = MASTERCARD;
+        } else if (s.length() == 16 && s.startsWith("4")) {
+            cardType = VISA;
+        } else if (s.length() == 14 && (s.startsWith("30") || s.startsWith("36") || s.startsWith("38"))) {
+            cardType = DINERS;
+        } else if (s.length() == 15 && (s.startsWith("27") || s.startsWith("37"))) {
+            cardType = AMERICAN_EXPRESS;
+        } else {
+            cardType = NULL;
+        }
+
+        return cardType;
+    }
+
+    private void onFinish() {
+        if (isValidCardType() && isValidCVV() && isValidUserName() && isValidIdNumber() && isValidEmail()){
+            Toast.makeText(this, "go to next page", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isValidCardType() {
+        if (getCardType(binder.creditCardCardNumber.getText().toString()) == NULL) {
+            onError(binder.creditCardCardNumber, "מספר כרטיס אשראי לא תקין");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean isValidCVV() {
+        if (binder.creditCardCardCVV.getText().toString().length() != 3) {
+            onError(binder.creditCardCardCVV, "מספר קוד אבטחה אינו תקין");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean isValidUserName() {
+        if (binder.creditCardPersonName.getText().toString().equals("")) {
+            onError(binder.creditCardPersonName, "שם משתמש חסר");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean isValidIdNumber() {
+        int idNumberLength = binder.creditCardPersonID.getText().toString().length();
+        if (idNumberLength > 9 || idNumberLength < 8) {
+            onError(binder.creditCardPersonID, "מספר זהות לא תקין");
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean isValidEmail() {
+        String email = binder.creditCardUserMail.getText().toString();
+        boolean isValid = !TextUtils.isEmpty(email) && (Patterns.EMAIL_ADDRESS.matcher(email).matches());
+
+        if (!isValid)
+            onError(binder.creditCardUserMail, "כתובת אימייל לא נכונה");
+
+        return isValid;
+    }
+
+    private void onError(EditText editText, String errorMessage) {
+        editText.setTextColor(getResources().getColor(R.color.red_error_phone, getTheme()));
+        editText.setText(errorMessage);
+        ((ScrollView)binder.getRoot()).smoothScrollTo(0, editText.getTop() - 30);
+        editText.requestFocus();
     }
 }
