31e803b98c7c9474c8c314a9c7c8d56c45514203
DESKTOP-76T2UMK\User\marmormoshe <moshe@ballaba-it.com>
2018-06-04 11:25:57 +0300

    card number icon validation completed

--- app/src/main/java/com/example/michaelkibenko/ballaba/Activities/CreditCardActivity.java
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Activities/CreditCardActivity.java
@@ -6,8 +6,11 @@ import android.databinding.DataBindingUtil;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.os.Message;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.IntDef;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
@@ -16,6 +19,15 @@ import com.example.michaelkibenko.ballaba.Common.BallabaDialogBuilder;
 import com.example.michaelkibenko.ballaba.R;
 import com.example.michaelkibenko.ballaba.Utils.UiUtils;
 import com.example.michaelkibenko.ballaba.databinding.ActivityCreditCardBinding;
+
+import static com.example.michaelkibenko.ballaba.Activities.CreditCardActivity.CREDIT_CARD_ICON.AMERICAN_EXPRESS;
+import static com.example.michaelkibenko.ballaba.Activities.CreditCardActivity.CREDIT_CARD_ICON.DINERS;
+import static com.example.michaelkibenko.ballaba.Activities.CreditCardActivity.CREDIT_CARD_ICON.ISRACARD;
+import static com.example.michaelkibenko.ballaba.Activities.CreditCardActivity.CREDIT_CARD_ICON.MASTERCARD;
+import static com.example.michaelkibenko.ballaba.Activities.CreditCardActivity.CREDIT_CARD_ICON.VISA;
+import static com.example.michaelkibenko.ballaba.Managers.BallabaSearchPropertiesManager.LAZY_LOADING_OFFSET_STATES.AFTER_20;
+import static com.example.michaelkibenko.ballaba.Managers.BallabaSearchPropertiesManager.LAZY_LOADING_OFFSET_STATES.FIRST_20;
+import static java.sql.Types.NULL;
 
 /**
  * Created by User on 03/06/2018.
@@ -57,10 +69,37 @@ public class CreditCardActivity extends BaseActivityWithActionBar implements Vie
         Toast.makeText(this, "go to next page", Toast.LENGTH_SHORT).show();
     }
 
+    @IntDef({ISRACARD, MASTERCARD, VISA, DINERS, AMERICAN_EXPRESS, NULL})
+    @interface CREDIT_CARD_ICON {
+        int ISRACARD = R.drawable.bathtub_blue_36;
+        int MASTERCARD = R.drawable.bed_blue_36;
+        int VISA = R.drawable.calender_blue_24_copy;
+        int DINERS = R.drawable.date_blue_36;
+        int AMERICAN_EXPRESS = R.drawable.exclusive;
+    }
     @Override
     public void onFocusChange(View v, boolean hasFocus) {
         if (!hasFocus) {
+            String s = binder.creditCardCardNumber.getText().toString().trim();
+            int cardType;
+            if (s.length() == 8 || s.length() == 9){
+                cardType = ISRACARD;
+            } else if (s.length() == 16 && (s.startsWith("51") || s.startsWith("52") || s.startsWith("53") || s.startsWith("54") || s.startsWith("55"))){
+                cardType = MASTERCARD;
+            } else if (s.length() == 16 && s.startsWith("4")) {
+                cardType = VISA;
+            } else if (s.length() == 14 && (s.startsWith("30") || s.startsWith("36") || s.startsWith("38"))) {
+                cardType = DINERS;
+            } else if (s.length() == 15 && (s.startsWith("27") || s.startsWith("37"))) {
+                cardType = AMERICAN_EXPRESS;
+            } else {
+                cardType = NULL;
+            }
 
+            //TESTING:
+            //Log.d(TAG, "credit number: " + s + " ,card type: " + cardType);
+
+            binder.creditCardCardNumber.setCompoundDrawablesRelativeWithIntrinsicBounds(0, 0, cardType, 0);
         }
     }
 }
