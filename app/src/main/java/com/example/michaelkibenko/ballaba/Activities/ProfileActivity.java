2d1c717c5a5828a7a3bc77e49982f3eee48e152a
DESKTOP-76T2UMK\User\marmormoshe <moshe@ballaba-it.com>
2018-06-21 17:01:38 +0300

    profile screen: email check, json, address, social, about, duplicating fields

--- app/src/main/java/com/example/michaelkibenko/ballaba/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/michaelkibenko/ballaba/Activities/ProfileActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
@@ -12,12 +13,17 @@ import android.support.design.widget.BottomSheetDialog;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.util.Patterns;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.michaelkibenko.ballaba.Entities.BallabaUser;
@@ -27,12 +33,15 @@ import com.example.michaelkibenko.ballaba.R;
 import com.example.michaelkibenko.ballaba.Utils.UiUtils;
 import com.example.michaelkibenko.ballaba.databinding.ActivityProfileBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 import static com.example.michaelkibenko.ballaba.Fragments.AddProperty.AddPropLandlordFrag.REQUEST_CODE_CAMERA;
 import static com.example.michaelkibenko.ballaba.Fragments.AddProperty.AddPropLandlordFrag.REQUEST_CODE_GALLERY;
 
-public class ProfileActivity extends BaseActivityWithActionBar implements View.OnClickListener{
+public class ProfileActivity extends BaseActivityWithActionBar implements View.OnClickListener, TextWatcher{
     private final int REQUEST_CODE_CREDIT_CARD = 3;
     private final String TAG = ProfileActivity.class.getSimpleName();
 
@@ -44,6 +53,8 @@ public class ProfileActivity extends BaseActivityWithActionBar implements View.O
         binder = DataBindingUtil.setContentView(this, R.layout.activity_profile);
 
         initViews();
+
+        //if we want edit mode not as default:
         /*binder.profileActivityFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -65,18 +76,27 @@ public class ProfileActivity extends BaseActivityWithActionBar implements View.O
         BallabaUser user = BallabaUserManager.getInstance().getUser();
         if (user != null) {
             Glide.with(this).load(user.getProfile_image()).into(binder.profileActivityImageAvatar);
-            binder.profileActivityDetailsFullName.setText(user.getFirst_name() + " " + user.getLast_name());
+            binder.profileActivityDetailsFirstName.setText(user.getFirst_name());
+            binder.profileActivityDetailsLastName.setText(user.getLast_name());
             binder.profileActivityDetailsPhone.setText(user.getPhone());
             binder.profileActivityDetailsDateOfBirth.setText(user.getBirth_date());
-            //binder.profileActivityDetailsProfession.setText(/*TODO get from server*/);
+            binder.profileActivityDetailsProfession.setText(null/*TODO get from server*/);
             binder.profileActivityStatusSpinner.setPrompt(null/*TODO get from server*/);
             binder.profileActivityChildrenSpinner.setPrompt(null/*TODO get from server*/);
+            binder.profileActivityDetailsCity.setText(user.getCity());
+            binder.profileActivityDetailsAddress.setText(user.getAddress());
+            binder.profileActivityDetailsStreetNo.setText(user.getStreet_number());
+            binder.profileActivityDetailsAptNo.setText(user.getApt_no());
             binder.profileActivityDetailsAddress.setText(user.getAddress());
             binder.profileActivityDetailsEmail.setText(user.getEmail());
             binder.profileActivityDetailsAbout.setText(user.getAbout());
-            binder.profileActivityDetailsCreditCard.setText(/*TODO get from server last 4 digits*/"**** - **** - **** - " + null);
+            binder.profileActivityDetailsCreditCard.setText(/*TODO get from server last 4 digits*/"**** - **** - **** - ****");
         }
-        UiUtils.instance(true, this).initAutoCompleteCity(binder.profileActivityDetailsAddress);
+
+        binder.profileActivityDetailsAbout.addTextChangedListener(this);
+        binder.profileActivityButtonNext.setOnClickListener(this);
+        UiUtils.instance(true, this).initAutoCompleteCity(binder.profileActivityDetailsCity);
+        UiUtils.instance(true, this).initAutoCompleteAddressInCity(binder.profileActivityDetailsAddress, binder.profileActivityDetailsCity);
     }
 
     private View sheetView;
@@ -101,8 +121,7 @@ public class ProfileActivity extends BaseActivityWithActionBar implements View.O
         boolean isValid = !TextUtils.isEmpty(email)
                 && (Patterns.EMAIL_ADDRESS.matcher(email).matches());
         if (!isValid) {
-            binder.profileActivityDetailsEmail.setTextColor(getResources().getColor(R.color.red_error_phone));
-            binder.profileActivityDetailsEmail.setText("invalid email address");
+            binder.profileActivityDetailsEmail.setError("כתובת דואר אלקטרוני שגויה");
             ((ScrollView) binder.getRoot()).smoothScrollTo(0, binder.profileActivityDetailsEmail.getTop() - 30);
             binder.profileActivityDetailsEmail.requestFocus();
         }
@@ -139,9 +158,41 @@ public class ProfileActivity extends BaseActivityWithActionBar implements View.O
                 }
                 break;
 
-            /*case R.id.profileActivity_button_next:
-                onFinish(ConnectionsManager.newInstance(this));*/
+            case R.id.profileActivity_button_next:
+                if (isEmailValid(binder.profileActivityDetailsEmail.getText().toString()))
+                    onFinish(ConnectionsManager.newInstance(this), getDataFromEditTexts(new JSONObject()));
         }
+    }
+
+    private void onFinish(ConnectionsManager connectionsManager,  JSONObject jsonObject) {
+
+        finish();
+    }
+    private JSONObject getDataFromEditTexts(JSONObject jsonObject) {
+        try {
+            //TODO set tags
+            byte[] bytes = UiUtils.instance(true, this).drawableToUri(binder.profileActivityImageAvatar.getDrawable());
+            jsonObject.put(binder.profileActivityImageAvatar.getTag()+"", bytes);
+            jsonObject.put(binder.profileActivityDetailsFirstName.getTag()+"", binder.profileActivityDetailsFirstName.getText());
+            jsonObject.put(binder.profileActivityDetailsLastName.getTag()+"", binder.profileActivityDetailsLastName.getText());
+            jsonObject.put(binder.profileActivityDetailsProfession.getTag()+"", binder.profileActivityDetailsProfession.getText());
+            jsonObject.put(binder.profileActivityStatusSpinner.getTag()+"", binder.profileActivityStatusSpinner.getPrompt());
+            jsonObject.put(binder.profileActivityChildrenSpinner.getTag()+"", binder.profileActivityChildrenSpinner.getPrompt());
+            jsonObject.put(binder.profileActivityDetailsCity.getTag()+"", binder.profileActivityDetailsCity.getText());
+            jsonObject.put(binder.profileActivityDetailsAddress.getTag()+"", binder.profileActivityDetailsAddress.getText());
+            jsonObject.put(binder.profileActivityDetailsStreetNo.getTag()+"", binder.profileActivityDetailsStreetNo.getText());
+            jsonObject.put(binder.profileActivityDetailsAptNo.getTag()+"", binder.profileActivityDetailsAptNo.getText());
+            jsonObject.put(binder.profileActivityDetailsEmail.getTag()+"", binder.profileActivityDetailsEmail.getText());
+            jsonObject.put(binder.profileActivityDetailsAbout.getTag()+"", binder.profileActivityDetailsAbout.getText());
+            jsonObject.put(binder.profileActivityDetailsFirstName.getTag()+"", binder.profileActivityDetailsFirstName.getText());
+            jsonObject.put(binder.profileActivityDetailsFirstName.getTag()+"", binder.profileActivityDetailsFirstName.getText());
+            return jsonObject;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
     @Override
@@ -193,5 +244,14 @@ public class ProfileActivity extends BaseActivityWithActionBar implements View.O
                 sheetView.findViewById(R.id.takePic_button_gallery).performClick();
             }
         }
+    }
+
+    @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+    @Override public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        binder.profileActivityDetailsAboutCounter.setText(s.length() + " / 120");
+
     }
 }
